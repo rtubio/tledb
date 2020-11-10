2bb2b12ae638c0d8152d0dbf1306b9d4944b51f9
Ricardo Tubio <rtpardavila@gmail.com>
2020-11-10 18:38:31 +0900

    Adding workflow env

--- .github/setup.sh
+++ .github/setup.sh
@@ -14,26 +14,12 @@ source "$gen_django_skg_sh"
 source "$gen_mail_file"
 source "$gen_celery_conf"
 
-source "$setup_dev_env"
-source "$setup_os_packages"
-
-echo "「インフォ」Setting up local development environment..."
-
 # 0) Create required directories
 mkdir -p "$SECRETS_D"
 mkdir -p "$LOGS_D"
 mkdir -p "$STATIC_D"
 
-# 1) A development environment is created, although in the docker container it will not be necessary.
-[[ ! -f "$VENV_ACTIVATE" ]] && {
-    echo "「インフォ」Python environment unavailable, installing..."
-    setup_os_packages "$PACKAGES_FILE"
-    setup_dev_env "$REQUIREMENTS_FILE" "$VENV_D" "$VENV_ACTIVATE"
-}
-
 # 1) Configure Django
-source "$VENV_ACTIVATE"
 create_secret_django "$DJANGO_SECRETS" "$( python $gen_django_skg_py )"
 create_secret_mail "$MAIL_SECRETS" 'localhost' '6666' 'test' 'test'
 create_celery_conf "$DJANGO_CELERY_CONF"
-deactivate
--- conf/dev/gen-ghenv.sh
+++ conf/dev/gen-ghenv.sh
@@ -1,6 +1,1 @@
 #!/bin/bash
-
-source 'conf/paths.conf'
-
-echo "tledb_py_pkgs=$REQUIREMENTS_FILE"
-echo "tledb_coverage_rc=$COVERAGE_CONF"
